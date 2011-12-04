25864699547a6c4e1510c12c01aadee1bc0f19ed
frederic.lecointre@gmail.com <frederic.lecointre@gmail.com@802914c9-5022-9226-5c50-2b098cbb32cc>
2011-12-04 16:11:47 +0000

    - Removed Color.BlendChannels - Added Libnoise.Lerp(byte, byte, float), Color.BlendChannels replacement - Added Color.Lerp24,  Color.Lerp32 and  Color.Lerp(IColor, IColor, float, bool). Provide lerp with or without alpha channel - Added convenient Color constructors - Added Color.Grayscale with some strategies

---  libnoise-dotnet/libnoise/src/Renderer/Color.cs
+++  libnoise-dotnet/libnoise/src/Renderer/Color.cs
@@ -35,24 +35,31 @@ namespace Graphics.Tools.Noise.Renderer {
 		#region fields
 
 		/// <summary>
+		/// Compute a grayscale value from the source color
+		/// </summary>
+		/// <param name="color">The source color</param>
+		/// <returns>The computed channel value</returns>
+		public delegate byte GrayscaleStrategy(IColor color);
+
+		/// <summary>
 		/// Value of the red channel
 		/// </summary>
-		protected byte _red;
+		protected byte _red = 0;
 
 		/// <summary>
 		/// Value of the green channel
 		/// </summary>
-		protected byte _green;
+		protected byte _green = 0;
 
 		/// <summary>
 		/// Value of the blue channel
 		/// </summary>
-		protected byte _blue;
+		protected byte _blue = 0;
 
 		/// <summary>
 		/// Value of the alpha (transparency) channel.
 		/// </summary>
-		protected byte _alpha;
+		protected byte _alpha = 255;
 
 		/// <summary>
 		/// Internal hashcode
@@ -114,21 +121,21 @@ namespace Graphics.Tools.Noise.Renderer {
 		}
 
 		/// <summary>
-		/// Create a red color
+		/// Create a solid red color
 		/// </summary>
 		public static Color RED {
 			get { return new Color(255, 0, 0, 255); }
 		}
 
 		/// <summary>
-		/// Create a green color
+		/// Create a solid green color
 		/// </summary>
 		public static Color GREEN {
 			get { return new Color(0, 255, 0, 255); }
 		}
 
 		/// <summary>
-		/// Create a blue color
+		/// Create a solid blue color
 		/// </summary>
 		public static Color BLUE {
 			get { return new Color(0, 0, 255, 255); }
@@ -146,18 +153,40 @@ namespace Graphics.Tools.Noise.Renderer {
 		#region Ctor/Dtor
 
 		/// <summary>
+		/// 0-args constructor, a solid black color (0, 0, 0, 255)
+		/// </summary>
+		public Color() {
+			_hashcode = (_red +_green +_blue) ^_rnd.Next();
+		}//end Color
+
+
+		/// <summary>
 		/// Create a new Color
 		/// </summary>
 		/// <param name="r">Value of the red channel</param>
 		/// <param name="g">Value of the green channel</param>
 		/// <param name="b">Value of the blue channel</param>
 		/// <param name="a">Value of the alpha channel</param>
-		public Color(byte r, byte g, byte b, byte a) {
+		public Color(byte r, byte g, byte b, byte a) 
+			:this(){
 			_red = r;
 			_green = g;
 			_blue = b;
 			_alpha = a;
-			_hashcode = (_red +_green +_blue) ^_rnd.Next();
+		}//end Color
+
+		/// <summary>
+		/// Create a new Color
+		/// </summary>
+		/// <param name="r">Value of the red channel</param>
+		/// <param name="g">Value of the green channel</param>
+		/// <param name="b">Value of the blue channel</param>
+		public Color(byte r, byte g, byte b)
+			: this() {
+			_red = r;
+			_green = g;
+			_blue = b;
+			_alpha = 255;
 		}//end Color
 
 		#endregion
@@ -180,37 +209,131 @@ namespace Graphics.Tools.Noise.Renderer {
 
 		#endregion
 
-		#region Interaction
-		/// <summary>
-		/// Performs linear interpolation between two 8-bit channel values.
-		/// </summary>
-		/// <param name="channel0">The first channel</param>
-		/// <param name="channel1">The second channel</param>
-		/// <param name="alpha">The alpha value</param>
-		/// <returns></returns>
-		public static Byte BlendChannel(byte channel0, byte channel1, float alpha){
-			float c0 = (float)channel0 / 255.0f;
-			float c1 = (float)channel1 / 255.0f;
-			return (byte)(((c1 * alpha) + (c0 * (1.0f - alpha))) * 255.0f);
-		}//end BlendChannel
+		#region Utilities
 
 		/// <summary>
-		/// Performs linear interpolation between two colors
+		/// Performs linear interpolation between two colors only with rgb channels
 		/// </summary>
-		/// <param name="color0"></param>
-		/// <param name="color1"></param>
-		/// <param name="alpha"></param>
-		/// <returns></returns>
-		public static IColor Lerp(IColor color0, IColor color1, float alpha) {
+		/// <param name="color0">The first color</param>
+		/// <param name="color1">The second color</param>
+		/// <param name="t">the amount to interpolate between the two colors</param>
+		/// <param name="withAlphaChannel">Flag indicates if this method also interpolate alpha channel</param>
+		/// <returns>The interpolated color, with the same type of color0</returns>
+		public static IColor Lerp(IColor color0, IColor color1, float t, bool withAlphaChannel) {
 
-			return new Color(
-				BlendChannel(color0.Red  , color1.Red  , alpha),
-				BlendChannel(color0.Green  , color1.Green  , alpha),
-				BlendChannel(color0.Blue  , color1.Blue  , alpha),
-				BlendChannel(color0.Alpha  , color1.Alpha  , alpha)
-			);
+			IColor color = (IColor)System.Activator.CreateInstance(color0.GetType());
+			color.Red = Libnoise.Lerp(color0.Red, color1.Red, t);
+			color.Green = Libnoise.Lerp(color0.Green, color1.Green, t);
+			color.Blue = Libnoise.Lerp(color0.Blue, color1.Blue, t);
+			color.Alpha = (withAlphaChannel) ? Libnoise.Lerp(color0.Alpha, color1.Alpha, t) : (byte)255;
+
+			return color;
 
 		}//end Lerp
+
+		/// <summary>
+		/// Performs linear interpolation between two colors, including alpha channel
+		/// </summary>
+		/// <param name="color0">The first color</param>
+		/// <param name="color1">The second color</param>
+		/// <param name="t">the amount to interpolate between the two colors</param>
+		/// <returns>The interpolated color</returns>
+		public static IColor Lerp(IColor color0, IColor color1, float t) {
+			return Lerp(color0, color1, t, true);
+		}//end Lerp
+
+		/// <summary>
+		/// Performs linear interpolation between two colors, including alpha channel
+		/// </summary>
+		/// <param name="color0">The first color</param>
+		/// <param name="color1">The second color</param>
+		/// <param name="t">the amount to interpolate between the two colors</param>
+		/// <returns>The interpolated color</returns>
+		public static IColor Lerp32(IColor color0, IColor color1, float t) {
+			return Lerp(color0, color1, t, true);
+		}//end Lerp
+
+		/// <summary>
+		/// Performs linear interpolation between two colors only with rgb channels
+		/// </summary>
+		/// <param name="color0">The first color</param>
+		/// <param name="color1">The second color</param>
+		/// <param name="t">the amount to interpolate between the two colors</param>
+		/// <returns>The interpolated color</returns>
+		public static IColor Lerp24(IColor color0, IColor color1, float t) {
+			return Lerp(color0, color1, t, false);
+		}//end Lerp
+
+		/// <summary>
+		/// Compute a grayscale value from the source color using Color.GrayscaleLuminosityStrategy.
+		/// </summary>
+		/// <param name="color">The source color</param>
+		/// <returns>The grayscale color with the same type as color</returns>
+		public static IColor Grayscale(IColor color) {
+
+			IColor returnColor = (IColor)System.Activator.CreateInstance(color.GetType());
+			returnColor.Red = returnColor.Green = returnColor.Blue = GrayscaleLuminosityStrategy(color);
+			returnColor.Alpha = 255;
+			return returnColor;
+
+		}//end public
+
+		/// <summary>
+		/// Compute a grayscale value from the source color using the given Strategy.
+		/// </summary>
+		/// <param name="color">The source color</param>
+		/// <param name="Strategy">The grayscale strategy</param>
+		/// <returns>The grayscale color with the same type as color</returns>
+		public static IColor Grayscale(IColor color, GrayscaleStrategy Strategy) {
+
+			IColor returnColor = (IColor)System.Activator.CreateInstance(color.GetType());
+			byte val = Strategy(color);
+			returnColor.Red = returnColor.Green = returnColor.Blue = val;
+			returnColor.Alpha = 255;
+			return returnColor;
+
+		}//end public
+
+		/// <summary>
+		/// A GrayscaleStrategy implementation
+		/// The lightness strategy averages the most prominent and least prominent colors: 
+		/// (max(R, G, B) + min(R, G, B)) / 2.
+		/// </summary>
+		/// <param name="color">The source color</param>
+		/// <returns>the computed channel value</returns>
+		public static byte GrayscaleLightnessStrategy(IColor color) {
+
+			return (byte)(
+				(
+					Math.Max(color.Red, Math.Max(color.Green, color.Blue)) +
+					Math.Min(color.Red, Math.Max(color.Green, color.Blue))
+				)/2
+			);
+
+		}//end GrayscaleLightnessStrategy
+
+		/// <summary>
+		/// A GrayscaleStrategy implementation
+		/// The average strategy simply averages the values: (R + G + B) / 3.
+		/// </summary>
+		/// <param name="color">The source color</param>
+		/// <returns>the computed channel value</returns>
+		public static byte GrayscaleAverageStrategy(IColor color) {
+			return (byte)((color.Red + color.Green + color.Blue) /3);
+		}//end GrayscaleAverageStrategy
+
+		/// <summary>
+		/// A GrayscaleStrategy implementation
+		/// The luminosity strategy averages the values, but it forms a weighted average to account 
+		/// for human perception. We’re more sensitive to green than other colors, 
+		/// so green is weighted most heavily. 
+		/// The formula for luminosity is 0.21f *R + 0.71f *G + 0.07f *B.
+		/// </summary>
+		/// <param name="color">The source color</param>
+		/// <returns>the computed channel value</returns>
+		public static byte GrayscaleLuminosityStrategy(IColor color) {
+			return (byte)(0.21f * color.Red + 0.71f * color.Green + 0.07f * color.Blue);
+		}//end GrayscaleLuminosityStrategy
 
 		#endregion
 
